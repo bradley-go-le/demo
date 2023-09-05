055c01b07cb29d03d9ac3a5ce24e4be70e9e458a
Bradley <bradley.le.goldenowl@gmail.com>
2023-09-05 14:12:57 +0700

    [Config] Class -> Module in constants initializers

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,23 +1,23 @@
-class APP
+module APP
   NAME = Rails.application.class.module_parent_name.downcase.freeze
 end
 
-class SIDEKIQ_QUEUES
+module SIDEKIQ_QUEUES
   SEND_EMAILS = "send_emails".freeze
   PRODUCER = "producer".freeze
 end
 
-class ACTION
+module ACTION
   UPDATE = :update!
   DESTROY = :destroy!
 end
 
 module TELEGRAM
-  class CODE
+  module CODE
     SUCCESS = [200, 201, 202]
   end
 
-  class END_POINTS
+  module END_POINTS
     SEND_MESSAGE = "https://api.telegram.org/bot#{ENV.fetch('BOT_TOKEN', nil)}/sendMessage"
     SEND_POLL = "https://api.telegram.org/bot#{ENV.fetch('BOT_TOKEN', nil)}/sendPoll"
     GET_UPDATES = "https://api.telegram.org/bot#{ENV.fetch('BOT_TOKEN', nil)}/getUpdates"
